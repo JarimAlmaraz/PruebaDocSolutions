b1f518cd820cdba5728189c53e8b6196d6302872
Alien 45 <Alien 45@LAPTOP-EATITUQE>
2022-08-12 18:47:04 -0500

    Agregar archivos de proyecto.

---
+++ Business/Usuario.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Usuario
+    {
+        public static Modelos.Result GetAll(Modelos.Usuario usuario)
+        {
+            Modelos.Result result = new Modelos.Result();
+
+            try
+            {
+                using (DataConnection.JAlmarazDOCSOLUTIONSEntities1 context = new DataConnection.JAlmarazDOCSOLUTIONSEntities1())
+                {
+                    if (usuario.Nombre == null)
+                    {
+                        usuario.Nombre = "";
+                    }
+
+                    var query = context.UsuarioGetAll(usuario.Nombre).ToList();
+
+                    result.Objects = new List<object>();
+
+                    if (query != null)
+                    {
+                        foreach (var objUsuario in query)
+                        {
+                            Modelos.Usuario user = new Modelos.Usuario();
+
+                            user.IdUsuario = objUsuario.IdUsuario;
+                            user.Nombre = objUsuario.Nombre;
+                            user.ApellidoPaterno = objUsuario.ApellidoPaterno;
+                            user.ApellidoMaterno = objUsuario.ApellidoMaterno;
+                            user.Edad = objUsuario.Edad.Value;
+                            user.FechaNacimiento = objUsuario.FechaNacimiento.Value;
+
+                            result.Objects.Add(user);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Error al obtener los datos de la Tabla Poliza";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        public static Modelos.Result GetById(int IdUsuario)
+        {
+            Modelos.Result result = new Modelos.Result();
+            try
+            {
+                using (DataConnection.JAlmarazDOCSOLUTIONSEntities1 context = new DataConnection.JAlmarazDOCSOLUTIONSEntities1())
+                {
+                    var objUsuario = context.UsuarioGetById(IdUsuario).First();
+
+                    if (objUsuario != null)
+                    {
+                        Modelos.Usuario usuario = new Modelos.Usuario();
+                        usuario.IdUsuario = objUsuario.IdUsuario;
+                        usuario.Nombre = objUsuario.Nombre;
+                        usuario.ApellidoPaterno = objUsuario.ApellidoPaterno;
+                        usuario.ApellidoMaterno = objUsuario.ApellidoMaterno;
+                        usuario.Edad = objUsuario.Edad.Value;
+                        usuario.FechaNacimiento = objUsuario.FechaNacimiento.Value;
+
+                        result.Object = usuario;
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Error al obtener la Poliza";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        public static Modelos.Result Add(Modelos.Usuario usuario)
+        {
+            Modelos.Result result = new Modelos.Result();
+            try
+            {
+                using (DataConnection.JAlmarazDOCSOLUTIONSEntities1 context = new DataConnection.JAlmarazDOCSOLUTIONSEntities1())
+                {
+                    var query = context.UsuarioAdd(usuario.Nombre, usuario.ApellidoPaterno, usuario.ApellidoMaterno, usuario.Edad, usuario.FechaNacimiento);
+
+                    if (query >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Ocurrio un error al agregar el nuevo usuario :C";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        public static Modelos.Result Update(Modelos.Usuario usuario)
+        {
+            Modelos.Result result = new Modelos.Result();
+            try
+            {
+                using (DataConnection.JAlmarazDOCSOLUTIONSEntities1 context = new DataConnection.JAlmarazDOCSOLUTIONSEntities1())
+                {
+                    var query = context.UsuarioUpdate(usuario.IdUsuario, usuario.Nombre, usuario.ApellidoPaterno, usuario.ApellidoMaterno, usuario.Edad, usuario.FechaNacimiento);
+                    if (query >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Ocurrio un error al actualizar el usuario :C";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        public static Modelos.Result Delete(int IdUsuario)
+        {
+            Modelos.Result result = new Modelos.Result();
+            try
+            {
+                using (DataConnection.JAlmarazDOCSOLUTIONSEntities1 context = new DataConnection.JAlmarazDOCSOLUTIONSEntities1())
+                {
+                    var query = context.UsuarioDelete(IdUsuario);
+                    if (query >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Ocurrio un error al eliminar el Usuario :C";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
+
---
+++ Presentation/Controllers/UsuarioController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+
+            Modelos.Result resultUsuario = Business.Usuario.GetAll(new Modelos.Usuario());
+
+            if (resultUsuario.Correct)
+            {
+                Modelos.Usuario usuario = new Modelos.Usuario();
+                usuario.Usuarios = resultUsuario.Objects;
+                return View(usuario);
+            }
+            else
+            {
+                ViewBag.Message = resultUsuario.ErrorMessage;
+                return PartialView("Modal");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Form(int? IdUsuario)
+        {
+            Modelos.Usuario usuario = new Modelos.Usuario();
+            if (IdUsuario == null)
+            {
+                return View(usuario);
+            }
+            else
+            {
+                Modelos.Result resultUsuario = Business.Usuario.GetById(IdUsuario.Value);
+                if (resultUsuario.Correct)
+                {
+                    usuario = ((Modelos.Usuario)resultUsuario.Object);
+                    return View(usuario);
+                }
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Form(Modelos.Usuario usuario)
+        {
+            Modelos.Result result = new Modelos.Result();
+            if (usuario.IdUsuario == 0) //ALTA DE NUEVOS USUARIO
+            {
+                result = Business.Usuario.Add(usuario);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "El usuario se agregó correctamente C:";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "El usuario no se agregó correctamente y existen los siguientes errores: " + result.ErrorMessage;
+                }
+            }
+            else //EDITAR DATOS DEL USUARIO
+            {
+                result = Business.Usuario.Update(usuario);
+
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "Usuario actualizado correctamente C:";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "El Usuario no se actualizó correctamente y existe el siguiente error: " + result.ErrorMessage;
+                }
+            }
+            return PartialView("Modal");
+        }
+
+        public ActionResult Delete(int IdUsuario)
+        {
+            Modelos.Result result = Business.Usuario.Delete(IdUsuario);
+
+            if (result.Correct)
+            {
+                ViewBag.Mensaje = "Usuario eliminado de manera correcta C:";
+            }
+            else
+            {
+                ViewBag.Mensaje = "Ocurrio un error al eliminar el usuario :C" + result.ErrorMessage;
+            }
+            return PartialView("Modal");
+        }
+        //FILTRADO
+        [HttpPost]
+        public ActionResult GetAll(Modelos.Usuario usuario)
+        {
+            Modelos.Result resultUsuarios = Business.Usuario.GetAll(usuario);
+
+            if (resultUsuarios.Correct)
+            {
+                usuario.Usuarios = new List<object>();
+                usuario.Usuarios = resultUsuarios.Objects;
+            }
+            else
+            {
+                ViewBag.Message = resultUsuarios.ErrorMessage;
+                return PartialView("Modal");
+            }
+            return View(usuario);
+        }
+    }
+}
